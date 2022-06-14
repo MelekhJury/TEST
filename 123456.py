d887ea3ec47be85b52d5414f57da465ac9c4dd26
MelekhJury <whitegold4864@gmail.com>
2022-06-14 21:52:19 +0300

    init function stroka_simvol(a, b) принимает две строки и возвращает большую(посимвольно) function stroka_lenght(a, b) принимает две строки и возвращает большую(длинна)

---
+++ 123456.py
@@ -0,0 +1,13 @@
+def stroka_simvol(a = "Dcs", b = "1234"):
+     if a > b:
+         return a
+     else:
+         return b
+print(stroka_simvol())
+
+def stroka_lenght(a = "Dcs", b = "1234"):
+    if len(a) > len(b):
+        return a
+    else:
+        return b
+print(stroka_lenght())
